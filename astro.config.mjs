69e6f9534d4b9681e4f1242286cd4ce5acd986e1
tomrcc <tom@cloudcannon.com>
2024-04-16 10:57:10 +1200

    Use SvelteTest component on an Astro page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import react from '@astrojs/react';
 import bookshop from '@bookshop/astro-bookshop';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muggy-fish.cloudvent.net/',
-  integrations: [mdx(), react(), tailwind(), bookshop()],
+  integrations: [svelte(), mdx(), react(), tailwind(), bookshop()],
 });
---
+++ src/components/SvelteTest.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let message;
+</script>
+
+<div>
+  <h1 class="bg-red-500">{message}</h1>
+</div>
