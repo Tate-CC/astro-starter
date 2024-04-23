2350d19abdeaa9b1627810ba3221c7ac83a3409a
tomrcc <tom@cloudcannon.com>
2024-04-23 13:29:16 +1200

    Remove manual imports in favour of autoimport

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,23 @@ import react from '@astrojs/react';
 import bookshop from '@bookshop/astro-bookshop';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import remarkAutoImport from '@cloudcannon/remark-auto-import';
 import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muggy-fish.cloudvent.net/',
   integrations: [mdx(), react(), tailwind(), bookshop(), alpine()],
+  markdown: {
+    remarkPlugins: [
+      [
+        remarkAutoImport,
+        {
+          directories: ['src/components/snippets'],
+          patterns: ['*.*'],
+        },
+      ],
+    ],
+    extendDefaultPlugins: true,
+  },
 });
